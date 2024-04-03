e60c6ca5a8b577305cbe0057dd5cf06d3f07ede2
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-04-02 19:17:56 -0500

    archivos de consutas de SQL agregados al directorio principal

--- consulta_RESERVES.sql
+++ consulta_RESERVES.sql
@@ -1,11 +1,12 @@
 SELECT * FROM reservationApp.RESERVES;
 
 SELECT RESERVES.reserve_id, RS.status_name as reserve_status, 
-RESERVES.user_id, USERS.name, USERS.lastname, ROOMS.room_id, ROS.status_name as room_status, 
+RESERVES.user_id, USERS.name, USERS.lastname, ROOMS.room_id, ROOMS.price, CURRENCY.currency_name, ROS.status_name as room_status, 
 RESERVES.reserveDay, RESERVES.createdAt, RESERVES.updatedAt, RESERVES.checkIn, RESERVES.checkOut
 FROM RESERVES
 JOIN ROOMS_RESERVES ON ROOMS_RESERVES.reserve_id=RESERVES.reserve_id
 JOIN ROOMS ON ROOMS_RESERVES.room_id=ROOMS.room_id
 JOIN STATUS AS RS ON RS.status_id=RESERVES.status_id
 JOIN STATUS AS ROS ON ROS.status_id=ROOMS.status_id
-JOIN USERS ON USERS.user_id=RESERVES.user_id;
+JOIN USERS ON USERS.user_id=RESERVES.user_id
+JOIN CURRENCY on ROOMS.currency_id=CURRENCY.currency_id;
--- consulta_ROOMS.sql
+++ consulta_ROOMS.sql
@@ -3,4 +3,4 @@ SELECT * FROM reservationApp.ROOMS;
 SELECT ROOMS.room_id as room, STATUS.status_name as status, ROOMS.size, ROOMS.price, CURRENCY.currency_name, ROOMS.createdAt, ROOMS.updatedAt
 FROM ROOMS
 LEFT JOIN STATUS ON STATUS.status_id=ROOMS.status_id
-LEFT JOIN CURRENCY ON CURRENCY.currency_id=ROOMS.currency;
+LEFT JOIN CURRENCY ON CURRENCY.currency_id=ROOMS.currency_id;
